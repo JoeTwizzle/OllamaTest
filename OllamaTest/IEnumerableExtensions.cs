ea2461d6bf7b2cd3f2262b321e3add601ce0ebc7
JoeTwizzle <the3.14159268483279@gmail.com>
2025-02-26 10:17:36 +0100

    Initial commit

---
+++ OllamaTest/IEnumerableExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OllamaTest
+{
+    internal static class IEnumerableExtensions
+    {
+        /// <summary>Return index and the associated item.</summary>
+        /// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}" /> to return an element from.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="source" /> is <see langword="null" />.</exception>
+        public static IEnumerable<(int Index, TSource Item)> Index<TSource>(this IEnumerable<TSource> source)
+        {
+            ArgumentNullException.ThrowIfNull(source, nameof(source));
+
+            if (source is Array a && a.Length == 0)
+            {
+                return [];
+            }
+
+            return IndexIterator(source);
+        }
+
+        private static IEnumerable<(int Index, TSource Item)> IndexIterator<TSource>(IEnumerable<TSource> source)
+        {
+            int index = -1;
+            foreach (TSource element in source)
+            {
+                checked
+                {
+                    index++;
+                }
+
+                yield return (index, element);
+            }
+        }
+    }
+}
